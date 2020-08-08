b739092ef5bba56abb7f0efd96ac5cf653812251
Tom Allardice <tallardi@yahoo.co.uk>
2020-08-08 12:35:45 +0100

    updated api names

--- 2020-08-08-working-with-cloud-build/ans.sh
+++ 2020-08-08-working-with-cloud-build/ans.sh
@@ -1,2 +1,4 @@
-gcloud services enable cloud build
-gcloud services enable container registry
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable containerregistry.googleapis.com
+chmod +x quickstart.sh
+gcloud builds submit --tag gcr.io/${GOOGLE_CLOUD_PROJECT}/quickstart-image .
